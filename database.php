4d8eb9d1a05c8fcbca3e9c503e5a68bb7c091877
Tantowi <tantowibowo7@gmail.com>
2025-08-24 17:06:22 +0800

    Update base url for general setting

--- database.php
+++ database.php
@@ -3,12 +3,9 @@ return [
     'adapter' => [
         'driver' => 'Pdo_Mysql',
         'host' => 'localhost',
-        // 'database' => 'db_naivebayes',
-        // 'username' => 'root',
-        // 'password' => 'root',
-        'database' => 'dipx2368_naive_bayes',
-        'username' => 'dipx2368_user_naive_bayes',
-        'password' => 'ce}QCd?[uS;c',
+         'database' => 'db_naivebayes',
+         'username' => 'root',
+         'password' => 'root',
         'charset' => 'utf8'
     ]
 ];
